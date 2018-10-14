2541c118b7c37a764b9ab81d858623ede9a89b6a
Joonas Myhrberg <joonas@myhrberg.email>
2018-10-14 20:27:01 +0300

    Add basic brightness and on/off control

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/brutella/hc"
 	hcAccessory "github.com/brutella/hc/accessory"
 	"github.com/joonasmyhrberg/pilight/accessory"
+	rpio "github.com/stianeikeland/go-rpio"
 )
 
 func main() {
+
+	err := rpio.Open()
+	if err != nil {
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	coolLEDPin := rpio.Pin(18)
+	warmLEDPin := rpio.Pin(19)
+	coolLEDPin.Mode(rpio.Pwm)
+	warmLEDPin.Mode(rpio.Pwm)
+	// Only needed for one pin since they share the frequency
+	coolLEDPin.Freq(1280000)
+	coolLEDPin.DutyCycle(0, 100)
+	warmLEDPin.DutyCycle(0, 100)
 
 	info := hcAccessory.Info{
 		Name:  "LEDStrip",
@@ -19,6 +36,7 @@ func main() {
 	ledStrip.WhiteSpectrumBulb.ColorTemperature.SetMinValue(154)
 	ledStrip.WhiteSpectrumBulb.ColorTemperature.SetMaxValue(370)
 	ledStrip.WhiteSpectrumBulb.ColorTemperature.SetValue(262)
+	ledStrip.WhiteSpectrumBulb.On.SetValue(false)
 
 	config := hc.Config{}
 	t, err := hc.NewIPTransport(config, ledStrip.Accessory)
@@ -26,12 +44,22 @@ func main() {
 		log.Panic(err)
 	}
 
+	brightness := 0
+
 	ledStrip.WhiteSpectrumBulb.On.OnValueRemoteUpdate(func(on bool) {
-		log.Println("On", on)
+		if on {
+			coolLEDPin.DutyCycle(uint32(brightness), 100)
+			warmLEDPin.DutyCycle(uint32(brightness), 100)
+		} else {
+			coolLEDPin.DutyCycle(0, 100)
+			warmLEDPin.DutyCycle(0, 100)
+		}
 	})
 
-	ledStrip.WhiteSpectrumBulb.Brightness.OnValueRemoteUpdate(func(brightness int) {
-		log.Println("Brightness", brightness)
+	ledStrip.WhiteSpectrumBulb.Brightness.OnValueRemoteUpdate(func(newBrightness int) {
+		brightness = newBrightness
+		coolLEDPin.DutyCycle(uint32(brightness), 100)
+		warmLEDPin.DutyCycle(uint32(brightness), 100)
 	})
 
 	ledStrip.WhiteSpectrumBulb.ColorTemperature.OnValueRemoteUpdate(func(colorTemperature int) {
